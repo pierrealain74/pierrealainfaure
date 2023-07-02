3c9d760e45c29580a5eb1bfb852fcacafba9139a
pierre <jobrider@gmail.com>
2023-07-02 22:34:53 +0200

    rajout fleche scroll

--- functions.php
+++ functions.php
@@ -13,7 +13,16 @@ function pafchild_enqueue_styles() {
 
 }
 
+//Script via le scroll
 
+/* function enqueue_scriptScroll()
+{
+
+    wp_enqueue_script('simpleparallax', get_stylesheet_directory_uri() . '/node_modules/simple-parallax-js/dist/simpleParallax.min.js', array(), false, true);
+
+    wp_enqueue_script('simpleparallax-config', get_stylesheet_directory_uri() . '/js/simpleparallax.js', array(), false, true);
+}
+add_action('wp_footer', 'enqueue_scriptScroll'); */
 
 
 
--- page.php
+++ page.php
@@ -20,11 +20,29 @@ get_header();
 
 	<section id="logo">
 		<h1>pierrealainfaure.com</h1>
+		<i class="fa-solid fa-arrow-right"><i class="fa-solid fa-arrow-down"><span>scroll</span></i></i>
 	</section>
 
 
 </main><!-- #main -->
 
+<script>
+/* 
+window.addEventListener('wheel', function(event) {
+  event.preventDefault(); // Empêche le défilement par défaut de la page
+  
+  // Vérifie la direction du défilement
+  var delta = event.deltaY || event.detail || event.wheelDelta;
+  var direction = (delta > 0) ? 1 : -1;
+  
+  // Effectue le décalage de 300px
+  var currentTransform = parseFloat(bigcloud.style.transform.replace('translateX(', '').replace('px)', ''));
+  var newTransform = currentTransform + (direction * 300);
+  bigcloud.style.transform = 'translateX(' + newTransform + 'px)';
+}); */
+
+</script>
+
 <?php
 //get_sidebar();
-//get_footer();
+get_footer();
