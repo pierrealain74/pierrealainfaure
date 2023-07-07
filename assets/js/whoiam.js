bf1a444635d73ab249962e742f614577bd9ac98e
pierre <jobrider@gmail.com>
2023-07-07 10:29:44 +0200

    fin de conception on wheel, nvll conception via scroll

--- assets/js/whoiam.js
+++ assets/js/whoiam.js
@@ -19,14 +19,27 @@ function introToWhoiam() {
   
     sectionLogo.style.opacity = (opacity / 100).toFixed(2);
   
-  if (sectionLogo.style.opacity === '0') {
+  if (sectionLogo.style.opacity == '0') {
     
     //console.log('opacity 0');
+    sectionWhoiam.style.display = 'block';
+
+    window.addEventListener("scroll", function (event) {
+      document.body.style.overflowY = 'scroll';
+
+      /*
+      
+      FIN DU FICHIER WHOIAM : CHANGEMENT DE CONCEPTION......
+      
+      
+      */
+      
+    });
 
 
-    
 
-
+  }else if(sectionLogo.style.opacity !== '0') {
+    sectionWhoiam.style.display = 'none';
   }
 
 }
@@ -35,6 +48,7 @@ function introToWhoiam() {
 var delta = 0;
 var posMouse = document.querySelector('.posMouse');
 var sectionLogo = document.querySelector('section#logo');
+var sectionWhoiam = document.querySelector('section#whoiam');
 var logoSection = document.querySelector('h1');
 var iconArrowRight = document.querySelector('.fa-arrow-right');
 var iconArrowDown = document.querySelector('.fa-arrow-down');
